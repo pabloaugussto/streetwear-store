f0445e7f0fd210977438f86f821d6fe9073b32fc
Pablo Augusto <augustopablo99@gmail.com>
2026-05-09 13:33:43 -0300

    Projeto inicial da loja streetwear com Django

---
+++ produtos/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from .models import Produto
+
+def home(request):
+    produtos = Produto.objects.all()
+
+    return render(request, 'home.html', {
+        'produtos': produtos
+    })
